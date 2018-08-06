696348ea979191200888a5452cb7a6a21c455040
ripley <ripley_l@outlook.com>
2018-08-06 14:40:52 +0800

    完成拓扑排序

--- day04/2.cpp
+++ day04/2.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char const* argv[])
 {
+    vector<vector<int>> adjlist;
+    int indgree[1000] = { 0 };
+    stack<int> headvertex;
+    string s;
+    while (getline(cin, s)) {
+        istringstream line(s);
+        int n;
+        vector<int> v;
+        while (line >> n) {
+            v.push_back(n);
+            indgree[n - 1]++;
+        }
+        adjlist.push_back(v);
+    }
+
+    for (int i = 0; i < adjlist.size(); i++) {
+        if (indgree[i] == 0) {
+            headvertex.push(i + 1);
+        }
+    }
+
+    if (headvertex.empty()) {
+        cout << "Not DAG!" << endl;
+        system("pause");
+        return -1;
+    }
+
+    vector<int> toplist;
+    do {
+        int n = headvertex.top();
+        toplist.push_back(n);
+        headvertex.pop();
+        for (int i = 0; i < adjlist[n - 1].size(); i++) {
+            int x = adjlist[n - 1][i];
+            indgree[x - 1]--;
+            if (indgree[x - 1] == 0) {
+                headvertex.push(x);
+            }
+        }
+    } while (!headvertex.empty());
+
+    for (int i = 0; i < toplist.size(); i++) {
+        cout << toplist[i] << " ";
+    }
+    cout << endl;
 
     system("pause");
     return 0;
